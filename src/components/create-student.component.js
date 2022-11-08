6a1ccc8035ad4ff9a09017dafad17c1b98e1a9af
Andy <ander1386@gmail.com>
2022-11-08 10:24:07 -0500

    second commit

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,7 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class CreateStudent extends Component {
   render() {
-    return <div>create-student.component</div>;
+    return (
+      <div class="form-wrapper">
+        <Form>
+          <Form.Group controlId="Name">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group controlId="Email">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+          <Form.Group controlId="Name">
+            <Form.Label>Código</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Button
+            variant="dark"
+            size="lg"
+            block="block"
+            type="submit"
+            className="mt-4"
+          >
+            Crear Estudiantes
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
+
+

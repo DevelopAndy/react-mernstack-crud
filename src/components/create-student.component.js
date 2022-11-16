5b6402bba2142194cc85f226d47b0af764e10bcd
Andy <ander1386@gmail.com>
2022-11-16 14:48:36 -0500

    reparando el error

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
@@ -7,59 +6,76 @@ import axios from "axios";
 export default class CreateStudent extends Component {
   constructor(props) {
     super(props);
-    this.onChangeStudentName = this.onChangeStudentName.bind(this)
-    this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this)
-    this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
 
-    this.state = { 
-      name:"",
-      email:"",
-      rollno:"",
-     }
+    this.onChangeStudentName = this.onChangeStudentName.bind(this);
+    this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+    this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      name: "",
+      email: "",
+      rollno: "",
+    };
   }
 
   onChangeStudentName(e) {
-    this.setState({name: e.target.value})
+    this.setState({ name: e.target.value });
   }
   onChangeStudentEmail(e) {
-    this.setState({email: e.target.value})
+    this.setState({ email: e.target.value });
   }
   onChangeStudentRollno(e) {
-    this.setState({rollno: e.target.value})
+    this.setState({ rollno: e.target.value });
   }
+
   onSubmit(e) {
     e.preventDefault();
-    const StundentObject = {
+    const studentObject = {
       name: this.state.name,
       email: this.state.email,
       rollno: this.state.rollno,
-    }
+    };
 
     axios
-    .post("http//:localhost:4000/students/create-student", StundentObject)
-    .then((res) => console.log(res.data));
-    this.setState({name: "", email: "", rollno: ""});
+      .post("http://localhost:4000/students/create-student", studentObject)
+      .then((res) => console.log(res.data));
+    this.setState({ name: "", email: "", rollno: "" });
   }
-  
+
   render() {
     return (
-      <div class="form-wrapper">
+      <div className="form-wrapper">
         <Form onSubmit={this.onSubmit}>
           <Form.Group controlId="Name">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName}/>
+            <Form.Control
+              type="text"
+              value={this.state.name}
+              onChange={this.onChangeStudentName}
+            />
           </Form.Group>
+
           <Form.Group controlId="Email">
             <Form.Label>Correo Electrónico</Form.Label>
-            <Form.Control type="email" value={this.state.email} onChange={this.onChangeStudentEmail} />
+            <Form.Control
+              type="email"
+              value={this.state.email}
+              onChange={this.onChangeStudentEmail}
+            />
           </Form.Group>
+
           <Form.Group controlId="Name">
             <Form.Label>Código</Form.Label>
-            <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeStudentRollno} />
+            <Form.Control
+              type="text"
+              value={this.state.rollno}
+              onChange={this.onChangeStudentRollno}
+            />
           </Form.Group>
+
           <Button
-            variant="dark"
+            variant="danger"
             size="lg"
             block="block"
             type="submit"
@@ -72,5 +88,3 @@ export default class CreateStudent extends Component {
     );
   }
 }
-
-
